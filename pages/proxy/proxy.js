31bfe339ca5ddd65fab66fc0b62fb4e39f4bfa32
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2018-05-20 12:59:41 +0800

    5.20 proxy

--- pages/proxy/proxy.js
+++ pages/proxy/proxy.js
@@ -2,16 +2,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    src: '/images/test-qr.png'
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
@@ -19,7 +10,7 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -40,13 +31,5 @@ Page({
         }
       })
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
   }
 })

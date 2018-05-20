59c83ed992e8341c90cc6082e493f9c34ec0dffc
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2018-05-20 14:00:25 +0800

    5.20index

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,13 +2,40 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000,
+    icons: [
+      {
+        src: '/images/icon-product.png',
+        text: '熊熊产品'
+      },
+      {
+        src: '/images/icon-food.png',
+        text: '美食详情'
+      },
+      {
+        src: '/images/icon-cartoon.png',
+        text: '漫画故事'
+      },
+      {
+        src: '/images/icon-video.png',
+        text: '试吃视频'
+      }
+    ],
+    iconNotice:'/images/icon-notice.png',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -19,7 +46,7 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -41,7 +68,7 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({

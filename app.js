0680bf99cf6cd3eea51dade01c3fc7c8f0cc0c07
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2018-05-27 09:20:08 +0800

    5.27 shop layout

--- app.js
+++ app.js
@@ -33,8 +33,41 @@ App({
       }
     })
   },
+  getUserInfo:function(cb){
+    var that = this
+    wx.login({
+      success: function () {
+        wx.getUserInfo({
+          success: function (res) {
+            that.globalData.userInfo = res.userInfo
+            console.log(res.userInfo);
+            typeof cb == "function" && cb(that.globalData.userInfo)
+          }
+        })
+      }
+    })
+  },
+  getSys:function() {
+    var that = this;
+    //  这里要非常注意，微信的scroll-view必须要设置高度才能监听滚动事件，所以，需要在页面的onLoad事件中给scroll-view的高度赋值
+    wx.getSystemInfo({
+      success: function(res) {
+        console.log(res.model)
+        console.log(res.pixelRatio)
+        console.log(res.windowWidth)
+        console.log(res.windowHeight)
+        console.log(res.language)
+        console.log(res.version)
+        console.log(res.platform)
+//设置变量值
+        that.globalData.sysInfo=res;
+        that.globalData.windowW=res.windowWidth;
+        that.globalData.windowH=res.windowHeight;
+      }
+    })
+  },
   globalData: {
-    APIHost: 'http://120.79.187.171:8888/api/',
+    APIHost: 'https://www.creaformation.cn/tools/submit_ajax.ashx',
     userInfo: null
   }
 })
--- pages/shop/shop.js
+++ pages/shop/shop.js
@@ -2,14 +2,17 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
+    category: [],
+    tabList: [],
+    cart: [],
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-  bindViewTap: function() {
-
-  },
   onLoad: function () {
+    const that = this
+    console.log(app.globalData.APIHost)
+    // that.setData({
+    //   windowH:sysInfo.windowHeight-44,
+    //   windoww:sysInfo.windowWidth
+    // });
   }
 })

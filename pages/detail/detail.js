3aab47e890500caf7661a7c82d1ca991a3435cf3
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2018-05-30 22:13:24 +0800

    5.27 shop layout

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,1 +1,21 @@
-Page({})
+const app = getApp()
+
+Page({
+  onLoad: function (e) {
+    this.fetchData(e.id)
+  },
+
+  fetchData(id){
+    wx.request({
+      url: app.globalData.APIHost,
+      method: 'GET',
+      data: {
+        action: 'article_info',
+        id: id
+      },
+      success: (res) => {
+        console.log(res)
+      }
+    })
+  }
+})

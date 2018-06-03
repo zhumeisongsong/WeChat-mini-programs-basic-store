ace45300d08efba7682e5b007ee443b6dfba1acc
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2018-06-03 22:30:28 +0800

    6.3 add one product and confirm order

--- pages/proxy/proxy.js
+++ pages/proxy/proxy.js
@@ -2,6 +2,22 @@ const app = getApp()
 
 Page({
   data: {
-    src: '/images/pic/test-qr.png'
+    host: app.globalData.Host,
+    src: ''
+  },
+  onLoad: function () {
+    wx.request({
+      url: app.globalData.APIHost,
+      method: 'GET',
+      data: {
+        action: 'get_imgpage'
+      },
+      success: res => {
+        console.log(res)
+        this.setData({
+          src: res.data.img_page
+        })
+      }
+    })
   }
 })
--- pages/shop/shop.js
+++ pages/shop/shop.js
@@ -8,7 +8,7 @@ Page({
     cart: [],
     count: 0,
     amount: 0,
-    modal: true,
+    modal: false,
     tabArr: {
       curHdIndex: 0,
       curBdIndex: 0
@@ -17,7 +17,6 @@ Page({
 
   onLoad: function () {
     this.fetchList()
-    // this.getCarts()
   },
 
   fetchList() {
@@ -47,22 +46,12 @@ Page({
               item.count = 0
             }
             this.setData({
-              ['tabList']: res.data.data
+              tabList: res.data.data
             })
           }
         })
       }
     })
-  },
-
-  addToCart(event) {
-    let id = event.currentTarget.dataset.id
-    this.setData({})
-  },
-
-  removeFromCart(event) {
-    let id = event.currentTarget.dataset.id
-    this.setData({})
   },
 
   tab(event) {
@@ -74,16 +63,63 @@ Page({
     this.setData({
       tabArr: obj
     })
-
   },
 
-  goConfirm() {
-    this.addToCart()
-    wx.navigateTo({
-      url: '../confirm/confirm'
+  addToCart(event) {
+    let item = event.currentTarget.dataset.item
+    let obj = this.data.cart
+    let isNew = true
+
+    if (item.count === 0) {
+      item.count = 1
+    }
+    let price = item.price / item.count
+    // let list = this.data.tabList
+    if (obj.length > 0) {
+      for (let i in obj) {
+        if (obj[i].id === item.id) {
+          obj[i].price = obj[i].price + price
+          obj[i].count = obj[i].count + 1
+          console.log(obj[i])
+          isNew = false
+        }
+      }
+    }
+
+    if (isNew) {
+      obj.push(item)
+    }
+
+    this.setData({
+      cart: obj,
+      // tabList: list,
+      count: this.data.count + 1,
+      amount: this.data.amount + price
     })
   },
+  removeFromCart(event) {
+    let item = event.currentTarget.dataset.item
+    let obj = this.data.cart
+    let price = item.price / item.count
 
+    console.log(item)
+
+    if (obj.length > 0) {
+      for (let i of obj) {
+        if (i.id === item.id && i.count > 0) {
+          i.price = i.price - price
+          i.count = i.count - 1
+        }
+      }
+      if (item.count > 1)
+        this.setData({
+          cart: obj,
+          // tabList: list,
+          count: this.data.count - 1,
+          amount: this.data.amount - price
+        })
+    }
+  },
   clearCart() {
     this.setData({
       cart: [],
@@ -91,9 +127,42 @@ Page({
       amount: 0
     })
   },
+
+  showModal() {
+    this.setData({
+      modal: true
+    })
+  },
   closeModal() {
     this.setData({
       modal: false
+    })
+  },
+
+  goConfirm() {
+    this.submitCart()
+    wx.navigateTo({
+      url: '../confirm/confirm'
+    })
+  },
+
+  submitCart(){
+    wx.request({
+      url: app.globalData.APIHost,
+      method: 'GET',
+      data: {
+        action: 'cart_goods_update',
+        guid: '',
+        goodis: '',
+        quantity: 1,
+      },
+      success: res => {
+        console.log(res)
+        // this.setData({
+        //   amount: res.data.amount,
+        //   count: res.data.num
+        // })
+      },
     })
   }
 })
@@ -144,22 +213,6 @@ Page({
 //   })
 // // },
 
-//
-// wx.request({
-//   url: app.globalData.APIHost,
-//   method: 'GET',
-//   data: {
-//     action: 'cart_goods_update',
-//     guid: res,
-//     goodis: id,
-//     quantity: 1,
-//   },
-//   success: res => {
-//     console.log(res)
-//     // this.setData({
-//     //   amount: res.data.amount,
-//     //   count: res.data.num
-//     // })
 
 // app.getUserOpenid((openid) => {
 //   this.setData({
